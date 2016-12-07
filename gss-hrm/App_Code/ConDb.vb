e68f74a6e14f625a9e1db061974467e3c5f5bae6
armmain <armmain@outlook.co.th>
2016-12-07 22:06:10 +0700

    add project gss-hrm

---
+++ gss-hrm/App_Code/ConDb.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Public Class ConDb
+    Dim Conn As SqlConnection = New SqlConnection(dbConn())
+    Public DA As SqlDataAdapter
+    Public DT As DataTable
+    Public Function dbConn() As String
+
+        Return ConfigurationManager.ConnectionStrings("HRM_GSS").ConnectionString
+
+    End Function
+
+    Public Function GetDataTable(ByVal StrSql As String, Optional ByVal TableName As String = "Table1") As DataTable
+        'Dim DA As New sqlDataAdapter(StrSql, dbConn())
+        DA = New SqlDataAdapter(StrSql, dbConn())
+        DT = New DataTable(TableName)
+        Try
+            DA.Fill(DT)
+            Return DT
+        Catch myerror As SqlException
+            Return Nothing
+        End Try
+
+    End Function
+
+End Class
---
+++ gss-hrm/Master/master_menu.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/Master/MasterPage.master" AutoEventWireup="false" CodeFile="master_menu.aspx.vb" Inherits="Master_master_menu" %>
+
+<%@ Register Assembly="DevExpress.Web.v16.1, Version=16.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web" TagPrefix="dx" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+        
+
+</asp:Content>
+
